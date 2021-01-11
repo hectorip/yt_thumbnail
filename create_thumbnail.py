5695787b3b213fac59570cd69eef8ade549dadc1
Héctor Iván Patricio Moreno <hectorivanpatriciomoreno@gmail.com>
2021-01-10 23:12:18 -0600

    WIP Create thumbnail

--- create_thumbnail.py
+++ create_thumbnail.py
@@ -3,7 +3,7 @@ import requests
 import datetime
 import os
 
-def download_image(keyword="abstract"):
+def download_image(keyword="space"):
     request = requests.get(
         "https://source.unsplash.com/1600x900/?"+keyword,
         allow_redirects=True
@@ -18,8 +18,5 @@ img.paste(bg)
 img = Image.eval(img, lambda x: x/2)
 d = ImageDraw.Draw(img)
 font =  ImageFont.truetype("/usr/share/fonts/truetype/open-sans/OpenSans-Bold.ttf", 104, encoding="unic")
-# d.rectangle(((0,0), (1280, 720)), fill=(0, 0, 0, 120),)
-d.text((100, 100), "Hola", fill='white', font=font, )
+d.text((30, 10), "Cómo me convertí en\ndesarrollador@", fill='white', font=font, )
 img.save("t.png")
-# img.show()
-# os.remove(bg_f)
